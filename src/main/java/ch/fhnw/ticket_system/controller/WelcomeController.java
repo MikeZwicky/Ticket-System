526c1c2451ee2ae9db5ae4fd3db2d5a2d9f6d17d
Noah Agostinis <noah.agostinis@students.fhnw.ch>
2025-06-09 13:33:58 +0000

    Welcome Controller Update

--- src/main/java/ch/fhnw/ticket_system/controller/WelcomeController.java
+++ src/main/java/ch/fhnw/ticket_system/controller/WelcomeController.java
@@ -1,7 +1,5 @@
 package ch.fhnw.ticket_system.controller;
 
-// import org.springframework.security.core.Authentication;
-// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +12,6 @@ public class WelcomeController {
     @GetMapping(value="/")
     public String getWelcomeString() {
         
-        return "Hello, welcome to our Pizzeria!";
+        return "Hello, welcome to our TicketSytem!";
     }
 }
